9800338bccfe46b7896ea4fc8eb928794f757835
armanot@gmail.com <armanot@gmail.com>
2022-03-10 20:13:17 +0800

    add new assets

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const path = require('path')
 const app = express()
 const port = 3003
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
+
+app.get('/mycal', (req, res) => {
+    res.sendFile(path.join(__dirname, '/cal2.html'))
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
